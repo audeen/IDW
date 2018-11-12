647439b6d5184640c1f61fc0d2d242303795aac8
audeen <jannik.sievert@gmail.com>
2018-11-12 10:52:05 +0100

    Aufgabe 4

---
+++ greet.js
@@ -0,0 +1,11 @@
+var welcome = document.getElementById('welcome');
+
+var name = "Klaus";
+var age = 32;
+
+welcome.innerHTML = "Hallo, " + name + " du bist " + age + " Jahre alt";  ;
+welcome.className = "vip";
+
+
+
+console.log('Willkommen in der Konsole');
