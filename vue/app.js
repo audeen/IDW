c28393e9747f370c90b7c8ebd8fcdaadf8355511
audeen <jannik.sievert@gmail.com>
2018-11-16 08:22:49 +0100

    aufgabe

--- vue/app.js
+++ vue/app.js
@@ -5,11 +5,24 @@ var app = new Vue({
     age: null,
     email: "",
 
-    contact: []
+    contact: [
+      { name: "peter", age: 23, email: "haha" },
+      { name: "klaus", age: 24, email: "huhu" }
+    ],
+
+    search: ""
   },
   methods: {
     push: function() {
-      this.contact.push({name: this.name, age: this.age, email: this.email});
+      this.contact.push({ name: this.name, age: this.age, email: this.email });
+    }
+  },
+  computed: {
+    filtered: function() {
+      var filteredlist = this.contact.filter(value => {
+        return value.name.includes(this.search);
+      });
+      return filteredlist;
     }
   }
 });
